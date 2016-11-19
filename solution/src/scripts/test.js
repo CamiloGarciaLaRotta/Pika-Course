1a85009d75bdc953f965a6ceef327b063f23c741
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2016-11-19 12:11:36 -0500

    fixed links to src

--- solution/src/scripts/test.js
+++ solution/src/scripts/test.js
@@ -1,5 +1,8 @@
 var students = require('../JSON/studentsByAvailability.json');
 var classes = require('../JSON/classes.json');
-
 //console.log(JSON.stringify(students,undefined,2))
-document.getElementById("user").innerHTML = JSON.stringify(students,undefined,2)
+
+
+function test(){
+    document.getElementById("user").innerHTML = JSON.stringify(students,undefined,2)
+}
